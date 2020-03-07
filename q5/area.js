a57463a0eaebd78761675f58b553b851c3a8426e
rajat-yadav-ttn <rajat.yadav@tothenew.com>
2020-03-07 20:57:17 +0530

    ES6.2

---
+++ q5/area.js
@@ -0,0 +1,5 @@
+
+const PI = 3.14159;
+export const areaCircle = r => PI * r * r;
+export const areaRectangle = (l, w) => l * w;
+export const areaCylinder = (r, l) => 2 * PI * r * l;
---
+++ q5/q5.js
@@ -0,0 +1,5 @@
+import {areaCircle, areaRectangle, areaCylinder} from './q5/area';
+
+console.log(`Area of circle with radius 2: ${areaCircle(2)}`);
+console.log(`Area of rectangle with l: 4, w: 3 ${areaRectangle(4, 3)}`);
+console.log(`Area of cylinder with radius 2, height: 5: ${areaCylinder(2, 5)}`);
